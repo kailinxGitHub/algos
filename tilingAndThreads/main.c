119e174b01f6be011aa450e35df4c3e784fc242f
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-08-07 21:41:20 -0400

    fix tiling and thread operation error

--- tilingAndThreads/main.c
+++ tilingAndThreads/main.c
@@ -3,13 +3,15 @@
 #include <pthread.h>
 #include <time.h>
 
-#define MAX_THREADS 4
+#define MAX_THREADS 8
 #define SIZE 512
 #define TILE_SIZE 512
 
 int arr1[SIZE][SIZE];
 int arr2[SIZE][SIZE];
 int result[SIZE][SIZE];
+
+int thread_args[MAX_THREADS][2];
 
 void randomIntArrGenerator(int lower, int upper, int size, int arr[size][size]) {
     for (int i = 0; i < size; i++) {
@@ -20,18 +22,14 @@ void randomIntArrGenerator(int lower, int upper, int size, int arr[size][size]) 
 }
 
 void *matrixMultiplicationThread(void *arg) {
-    long tid = *(long *)arg;
-    int num_tiles_per_thread = (SIZE / TILE_SIZE) / MAX_THREADS;
-    int start_tile = tid * num_tiles_per_thread;
-    int end_tile = (tid + 1) * num_tiles_per_thread;
+    int *data = (int *)arg;
+    int start_row = data[0];
+    int end_row = data[1];
 
-    clock_t start_time = clock();
-//    tiling algo for each row of first matrix and first column of second matrix for multiplication
-    for (int tile_row = start_tile; tile_row < end_tile; tile_row++) {
+    for (int tile_row = start_row; tile_row < end_row; tile_row++) {
         for (int tile_col = 0; tile_col < SIZE / TILE_SIZE; tile_col++) {
             for (int i = tile_row * TILE_SIZE; i < (tile_row + 1) * TILE_SIZE; i++) {
                 for (int j = tile_col * TILE_SIZE; j < (tile_col + 1) * TILE_SIZE; j++) {
-                    result[i][j] = 0;
                     for (int k = 0; k < SIZE; k++) {
                         result[i][j] += arr1[i][k] * arr2[k][j];
                     }
@@ -40,36 +38,42 @@ void *matrixMultiplicationThread(void *arg) {
         }
     }
 
-    clock_t end_time = clock();
-    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-    printf("Thread %d completed in %.10f seconds\n", tid, time_taken);
-
     pthread_exit(NULL);
 }
 
 int main() {
     srand(time(0));
 
-//    generating the random int arrays as matrices
+    // Generating the random int arrays as matrices
     randomIntArrGenerator(0, 100, SIZE, arr1);
     randomIntArrGenerator(0, 100, SIZE, arr2);
 
-//    creating the threads
-    pthread_t threads[MAX_THREADS];
-    int thread_ids[MAX_THREADS];
-
-//    running the multiplication
-    clock_t start_time = clock();
-    for (int i = 0; i < MAX_THREADS; i++) {
-        thread_ids[i] = i;
-        pthread_create(&threads[i], NULL, matrixMultiplicationThread, &thread_ids[i]);
+    // Initializing the result matrix to 0
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = 0;
+        }
     }
+
+    // Creating the threads
+    pthread_t threads[MAX_THREADS];
+    int rows_per_thread = SIZE / MAX_THREADS;
+
+    // Starting time measurement
+    double start_time = clock();
+
+    for (int i = 0; i < MAX_THREADS; i++) {
+        thread_args[i][0] = i * rows_per_thread / TILE_SIZE;
+        thread_args[i][1] = (i + 1) * rows_per_thread / TILE_SIZE;
+        pthread_create(&threads[i], NULL, matrixMultiplicationThread, (void *)thread_args[i]);
+    }
+
     for (int i = 0; i < MAX_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
-    clock_t end_time = clock();
 
+    // Ending time measurement
+    double end_time = clock();
     double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
     printf("---\n");
