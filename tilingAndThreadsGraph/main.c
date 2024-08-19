b2ce66f841696b709ae40e4b329c167f188f79e0
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-08-19 16:27:00 -0400

    update thread and tile

--- tilingAndThreadsGraph/main.c
+++ tilingAndThreadsGraph/main.c
@@ -3,15 +3,20 @@
 #include <pthread.h>
 #include <time.h>
 
-#define SIZE 512
-#define TILE_SIZE 512
+#define SIZE 512  // Matrix size
+#define TILE_SIZE 64  // Tile size to improve memory access
 
 int arr1[SIZE][SIZE];
 int arr2[SIZE][SIZE];
 int result[SIZE][SIZE];
 
-int thread_args[SIZE][2];
+// Struct to hold the start and end indices for each thread
+typedef struct {
+    int start_row;
+    int end_row;
+} ThreadData;
 
+// Function to generate random integer matrices
 void randomIntArrGenerator(int lower, int upper, int size, int arr[size][size]) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -20,48 +25,57 @@ void randomIntArrGenerator(int lower, int upper, int size, int arr[size][size]) 
     }
 }
 
+// Thread function to perform matrix multiplication for a portion of the matrix (based on tiles)
 void *matrixMultiplicationThread(void *arg) {
-    int *data = (int *)arg;
-    int start_row = data[0];
-    int end_row = data[1];
+    ThreadData *data = (ThreadData *)arg;
+    int start_row = data->start_row;
+    int end_row = data->end_row;
 
-    for (int tile_row = start_row; tile_row < end_row; tile_row++) {
-        for (int tile_col = 0; tile_col < SIZE / TILE_SIZE; tile_col++) {
-            for (int i = tile_row * TILE_SIZE; i < (tile_row + 1) * TILE_SIZE; i++) {
-                for (int j = tile_col * TILE_SIZE; j < (tile_col + 1) * TILE_SIZE; j++) {
+    // Perform matrix multiplication for the assigned rows (process in tiles)
+    for (int i = start_row; i < end_row; i += TILE_SIZE) {
+        for (int j = 0; j < SIZE; j += TILE_SIZE) {
+            for (int ii = i; ii < i + TILE_SIZE && ii < end_row; ii++) {
+                for (int jj = j; jj < j + TILE_SIZE && jj < SIZE; jj++) {
                     for (int k = 0; k < SIZE; k++) {
-                        result[i][j] += arr1[i][k] * arr2[k][j];
+                        result[ii][jj] += arr1[ii][k] * arr2[k][jj];
                     }
                 }
             }
         }
     }
-
     pthread_exit(NULL);
 }
 
+// Function to handle matrix multiplication with the given number of threads
 double matrixMultiplicationWithThreads(int num_threads) {
     pthread_t threads[num_threads];
+    ThreadData thread_data[num_threads];
+
     int rows_per_thread = SIZE / num_threads;
 
+    // Initialize the result matrix to 0
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             result[i][j] = 0;
         }
     }
 
+    // Start time measurement
     double start_time = clock();
 
+    // Create threads and assign them portions of the matrix
     for (int i = 0; i < num_threads; i++) {
-        thread_args[i][0] = i * rows_per_thread / TILE_SIZE;
-        thread_args[i][1] = (i + 1) * rows_per_thread / TILE_SIZE;
-        pthread_create(&threads[i], NULL, matrixMultiplicationThread, (void *)thread_args[i]);
+        thread_data[i].start_row = i * rows_per_thread;
+        thread_data[i].end_row = (i == num_threads - 1) ? SIZE : (i + 1) * rows_per_thread;
+        pthread_create(&threads[i], NULL, matrixMultiplicationThread, (void *)&thread_data[i]);
     }
 
+    // Join threads
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
+    // End time measurement
     double end_time = clock();
     return (double)(end_time - start_time) / CLOCKS_PER_SEC;
 }
@@ -69,20 +83,15 @@ double matrixMultiplicationWithThreads(int num_threads) {
 int main() {
     srand(time(0));
 
+    // Generate the random integer arrays (matrices)
     randomIntArrGenerator(0, 100, SIZE, arr1);
     randomIntArrGenerator(0, 100, SIZE, arr2);
 
-    FILE *fptr = fopen("threadScaling.csv", "w");
-
-    fprintf(fptr, "Threads,Time Taken\n");
-
-    for (int num_threads = 1; num_threads <= 32; num_threads *= 2) {
+    // Test with different thread counts
+    for (int num_threads = 1; num_threads <= 8; num_threads *= 2) {
         double time_taken = matrixMultiplicationWithThreads(num_threads);
         printf("Time taken with %d threads: %.10f seconds\n", num_threads, time_taken);
-        fprintf(fptr, "%d,%.10f\n", num_threads, time_taken);
     }
-
-    fclose(fptr);
 
     return 0;
 }
