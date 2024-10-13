2de0cb672985b067de32ce836046c093ac734e5c
Kailin <67965369+kailinxGitHub@users.noreply.github.com>
2024-10-12 22:31:13 -0400

    implement the standard matrix multiplication algorithm

---
+++ tilingAndThreads/cmake-build-debug/CMakeFiles/tilingAndThreads.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tilingAndThreads.dir/main.c.o"
+  "CMakeFiles/tilingAndThreads.dir/main.c.o.d"
+  "tilingAndThreads"
+  "tilingAndThreads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tilingAndThreads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tilingAndThreads/cmake-build-debug/CMakeFiles/tilingAndThreads.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tilingAndThreads.
+# This may be replaced when dependencies are built.
